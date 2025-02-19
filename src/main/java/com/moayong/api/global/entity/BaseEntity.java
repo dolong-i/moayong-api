4da608d3efe29257623894a05d609bb6a545973c
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-19 21:20:22 +0900

    Chore: 개발 환경 변동 파일

--- src/main/java/com/moayong/api/global/entity/BaseEntity.java
+++ src/main/java/com/moayong/api/global/entity/BaseEntity.java
@@ -2,16 +2,17 @@ package com.moayong.api.global.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
     @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
