9195e6692db918b0c263bbf6f071a6305011e315
itaebro <plus100kt@gmail.com>
2025-02-28 16:13:33 +0900

    fix(global): 에러핸들링 depth 및 메시지 변경

--- src/main/java/com/moayong/api/global/exception/DomainSpecificException.java
+++ src/main/java/com/moayong/api/global/exception/DomainSpecificException.java
@@ -7,10 +7,9 @@ import org.springframework.http.HttpStatus;
 public abstract class DomainSpecificException extends RuntimeException {
     private final String domain;
     private final HttpStatus status;
-    private final String customMessage;
 
-    protected DomainSpecificException(String domain, HttpStatus status, String customMessage) {
-        this.customMessage = customMessage;
+    protected DomainSpecificException(String domain, HttpStatus status, String message) {
+        super(message);
         this.status = status;
         this.domain = domain;
     }
--- src/main/java/com/moayong/api/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/moayong/api/global/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.stream.Collectors;
 
@@ -17,41 +19,48 @@ public class GlobalExceptionHandler {
     // 공통 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException ex) {
-        log.error("Validation error: {}", ex.getMessage(), ex);
+        // log.info("Validation error: {}", ex.getMessage(), ex);
         String errorMessage = ex.getBindingResult().getFieldErrors()
                 .stream()
                 .map(fieldError -> fieldError.getField() + " : " + fieldError.getDefaultMessage())
                 .collect(Collectors.joining(", "));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.error(HttpStatus.BAD_REQUEST.value(), "잘못된 입력값입니다."));
+                .body(ApiResponse.error(HttpStatus.BAD_REQUEST.value(), errorMessage));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse<?>> handleIllegalArgumentException(IllegalArgumentException ex) {
-        log.error("Illegal argument: {}", ex.getMessage(), ex);
+        // log.info("Illegal argument: {}", ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.error(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다."));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException ex) {
-        log.error("Runtime exception: {}", ex.getMessage(), ex);
+        log.info("Runtime exception: {}", ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 오류입니다."));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleNotFound(NoResourceFoundException ex) {
+        // log.info("handleNot found: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(HttpStatus.NOT_FOUND.value(), "존재하지 않는 경로입니다"));
     }
 
     // 도메인별 세부 예외 처리
     @ExceptionHandler(DomainSpecificException.class)
     public ResponseEntity<ApiResponse<?>> handleDomainSpecificException(DomainSpecificException ex) {
-        log.error("{} exception: {}", ex.getDomain(), ex.getMessage(), ex);
+        log.info("{} exception: {}", ex.getDomain(), ex.getMessage(), ex);
         return ResponseEntity.status(ex.getStatus())
-                .body(ApiResponse.error(ex.getStatus().value(), ex.getCustomMessage()));
+                .body(ApiResponse.error(ex.getStatus().value(), ex.getMessage()));
     }
 
     // 기타 미처리 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleUnexpectedException(Exception ex) {
-        log.error("Unexpected error: {}", ex.getMessage(), ex);
+        log.info("Unexpected error: {}", ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), "알 수 없는 오류"));
     }
