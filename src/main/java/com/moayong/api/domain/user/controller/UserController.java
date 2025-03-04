1033e2d44a4ca53f937207364154bc4a5fbd6a5c
itaebro <plus100kt@gmail.com>
2025-03-05 02:04:23 +0900

    fix(user): 쿠키 추가에 따른 변경 및 아이디 비교

--- src/main/java/com/moayong/api/domain/user/controller/UserController.java
+++ src/main/java/com/moayong/api/domain/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.moayong.api.domain.user.controller;
 
-import com.moayong.api.domain.auth.security.UserPrincipal;
+import com.moayong.api.domain.auth.config.UserPrincipal;
+import com.moayong.api.domain.auth.service.AuthService;
 import com.moayong.api.domain.user.domain.User;
 import com.moayong.api.domain.user.dto.response.UserResponse;
 import com.moayong.api.domain.user.service.UserService;
 import com.moayong.api.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,9 +20,12 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
+    private final AuthService authService;
 
-    @GetMapping("users/{id}")
-    public ApiResponse<UserResponse> findUserById(@PathVariable("id") Long id) {
+    @GetMapping("/users/{id}")
+    public ApiResponse<UserResponse> findUserById(@PathVariable("id") Long id,
+                                                  @AuthenticationPrincipal UserPrincipal principal) {
+        authService.validateUserAccess(id, Long.valueOf(principal.getUserId()));
         User user = userService.findUserById(id);
         UserResponse response = new UserResponse(user);
 
@@ -26,9 +33,11 @@ public class UserController {
     }
 
     @GetMapping("/users/me")
-    public ApiResponse<UserResponse> getCurrentUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        User user = userService.findUserById(userPrincipal.getUserId());
+    public ApiResponse<UserResponse> findCurrentUser(@AuthenticationPrincipal UserPrincipal principal) {
+        Long id = Long.valueOf(principal.getUserId());
+        User user = userService.findUserById(id);
         UserResponse response = new UserResponse(user);
+
         return ApiResponse.success(response, "유저 조회 성공");
     }
 
