5874ebc5c88f40701b523959c13177b1c5e100a3
itaebro <plus100kt@gmail.com>
2025-03-09 05:37:14 +0900

    feat(auth): LEAGUE_NOT_MATCH 에러 추가

--- src/main/java/com/moayong/api/domain/auth/enums/AuthErrorCode.java
+++ src/main/java/com/moayong/api/domain/auth/enums/AuthErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력 값입니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 아이디입니다."),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 유저입니다."),
     FAIL_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "로그인 정보가 정확하지 않습니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "권한이 없습니다"),
@@ -16,6 +16,7 @@ public enum AuthErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다"),
     UNSUPPORTED_TOKEN(HttpStatus.BAD_REQUEST, "JWT 형식이 다릅니다"),
     EMPTY_JWT_CLAIMS(HttpStatus.BAD_REQUEST, "JWT 토큰이 없습니다."),
+    LEAGUE_NOT_MATCH(HttpStatus.BAD_REQUEST, "리그멤버를 생성해주세요"),
     SUCCESS(HttpStatus.OK, "성공"),
     ;
 
