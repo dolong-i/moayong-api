981607ed960db8e940c73e8564afd2f37cf37023
itaebro <plus100kt@gmail.com>
2025-03-12 18:52:59 +0900

    fix(leagueMember): /api/v1 누락 수정

--- src/main/java/com/moayong/api/domain/leaguemember/controller/LeagueMemberController.java
+++ src/main/java/com/moayong/api/domain/leaguemember/controller/LeagueMemberController.java
@@ -7,12 +7,14 @@ import com.moayong.api.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class LeagueMemberController {
     private final LeagueMemberService memberService;
 
