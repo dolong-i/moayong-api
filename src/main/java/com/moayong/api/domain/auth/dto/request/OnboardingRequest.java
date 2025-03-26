77b46a8d8f05af815ec0583909b794894fc4e59a
truuuely <kmyc7030@gmail.com>
2025-03-26 17:01:26 +0900

    fix(user): 이름, 닉네임 입력 시 공백, 특수문자, 숫자 포함 불가

--- src/main/java/com/moayong/api/domain/auth/dto/request/OnboardingRequest.java
+++ src/main/java/com/moayong/api/domain/auth/dto/request/OnboardingRequest.java
@@ -5,13 +5,18 @@ import com.moayong.api.domain.user.enums.SavingsBank;
 import com.moayong.api.global.validation.ByteLength;
 import com.moayong.api.global.validation.SavingsRate;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record OnboardingRequest (
         @NotNull
+        @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "공백, 특수문자, 숫자는 포함될 수 없습니다.")
         String name,
+
         @NotNull
+        @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "공백, 특수문자, 숫자는 포함될 수 없습니다.")
         @ByteLength(min = 4, max = 16, message = "닉네임은 4바이트 이상 16바이트 이하여야 합니다.")
         String nickname,
+
         @NotNull
         SavingsBank savingsBank,
         @NotNull
--- src/main/java/com/moayong/api/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/moayong/api/domain/user/dto/request/UserUpdateRequest.java
@@ -4,9 +4,11 @@ import com.moayong.api.domain.user.domain.User;
 import com.moayong.api.global.validation.ByteLength;
 import com.moayong.api.global.validation.SavingsRate;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
 public record UserUpdateRequest(
+        @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "공백, 특수문자, 숫자는 포함될 수 없습니다.")
         @ByteLength(min = 4, max = 16, message = "닉네임은 4바이트 이상 16바이트 이하여야 합니다.")
         String nickname,
 
