d6bb3f7628a6f0513cfe97029b74de789fe9c5a9
truuuely <kmyc7030@gmail.com>
2025-03-26 13:20:48 +0900

    fix(league): 티어 수정

--- src/main/java/com/moayong/api/domain/league/enums/Tier.java
+++ src/main/java/com/moayong/api/domain/league/enums/Tier.java
@@ -2,22 +2,20 @@ package com.moayong.api.domain.league.enums;
 
 import lombok.Getter;
 
-import java.util.Arrays;
-
 @Getter
 public enum Tier {
-    BRONZE(1, "아가용", "https://d3t251u9x9cmf6.cloudfront.net/egg.png", 60, 101),
-    SILVER(2, "초딩용", "https://d3t251u9x9cmf6.cloudfront.net/baby-dragon.png", 30, 90),
-    GOLD(3, "중딩용", "https://d3t251u9x9cmf6.cloudfront.net/child-dragon.png", 20, 80),
-    PLATINUM(4, "고딩용", "https://d3t251u9x9cmf6.cloudfront.net/youth-dragon.png", 0, 60);
+    EGG(1, "알이용", "https://d3t251u9x9cmf6.cloudfront.net/egg-dragon.png", 60, 101),
+    BABY(2, "아기용", "https://d3t251u9x9cmf6.cloudfront.net/baby-dragon.png", 30, 90),
+    CHILD(3, "어린이용", "https://d3t251u9x9cmf6.cloudfront.net/child-dragon.png", 20, 80),
+    TEEN(4, "청소년용", "https://d3t251u9x9cmf6.cloudfront.net/young-dragon.png", 10, 60),
+    ADULT(5, "어른용", "https://d3t251u9x9cmf6.cloudfront.net/adult-dragon.png", 10, 60),
+    GOD(6, "짱이용", "https://d3t251u9x9cmf6.cloudfront.net/god-dragon.png", 0, 60);
 
     private final int level;
     private final String name;
     private final String imageUrl;
     private final int PromotionRate;
     private final int RelegationRate;
-    private final int FIRST = 1;
-    private final int LAST = 5;
 
     Tier(int level, String name, String imageUrl, int PromotionRate, int RelegationRate) {
         this.level = level;
@@ -25,26 +23,5 @@ public enum Tier {
         this.imageUrl = imageUrl;
         this.PromotionRate = PromotionRate;
         this.RelegationRate = RelegationRate;
-    }
-
-    public static Tier findByLevel(int level) {
-        return Arrays.stream(values())
-                .filter(tier -> tier.level == level)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Invallevel tier level: " + level));
-    }
-
-    public Tier getNextTier() {
-        if (this.level == LAST) {
-            return findByLevel(this.level);
-        }
-        return findByLevel(this.level + 1);
-    }
-
-    public Tier getPrevTier() {
-        if (this.level == FIRST) {
-            return findByLevel(this.level);
-        }
-        return findByLevel(this.level - 1);
     }
 }
